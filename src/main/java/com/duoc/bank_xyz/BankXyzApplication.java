0d395154223db742892d283beb23efad26973f0f
m-vargas-h <m.vargas@outlook.cl>
2026-08-12 19:16:31 -0400

    Initial commit

---
+++ src/main/java/com/duoc/bank_xyz/BankXyzApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.bank_xyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankXyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankXyzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/bank_xyz/BankXyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.bank_xyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankXyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
