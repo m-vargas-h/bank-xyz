bbcb9afb9cb85a38f67a87a2ab3b79a49d02d37c
m-vargas-h <m.vargas@outlook.cl>
2026-09-20 03:08:06 -0300

    chore: docker-compose, dockerfiles y limpieza de proyecto

--- bank-xyz-batch/src/main/java/com/duoc/bank_xyz_batch/exception/InvalidBankDataException.java
+++
@@ -1,7 +0,0 @@
-package com.duoc.bank_xyz_batch.exception;
-
-public class InvalidBankDataException extends RuntimeException {
-    public InvalidBankDataException(String message) {
-        super(message);
-    }
-}
